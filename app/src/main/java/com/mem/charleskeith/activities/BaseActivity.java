53324e6a21d5c287634249274fef56382aa3a875
myaeimyo <myaeimyo@gmail.com>
2018-06-25 23:34:21 +0630

    create package

---
+++ app/src/main/java/com/mem/charleskeith/activities/BaseActivity.java
@@ -0,0 +1,6 @@
+package com.mem.charleskeith.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+}
--- app/src/main/java/com/mem/charleskeith/activities/MainActivity.java
+++ app/src/main/java/com/mem/charleskeith/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.mem.charleskeith;
+package com.mem.charleskeith.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -9,7 +9,9 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.mem.charleskeith.R;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
